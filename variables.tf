955038e5c9cbeb5c68351aeb5a32ff78fa959367
pengjiang80 <peng.jiang@hotmail.com>
2023-09-01 11:05:33 +0800

    update

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-# @options ["c5.2xlarge","c5.4xlarge","g4dn.xlarge","g4dn.2xlarge","g4dn.4xlarge"]
+# @options ["c5.2xlarge","c5.4xlarge","t2.2xlarge", "t3.2xlarge"]
 variable "instance_type" {
   type = string
   description = "Instance type"
